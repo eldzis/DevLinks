c3e50c2749c8b29bcd8807bf3f24414d32288123
Elgars Grodnis <elgarsgrodnis@Elgarss-MBP.Dlink>
2025-10-28 16:16:58 +0200

    feat(Docker): Dockerize project for full docker development

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { LinksModule } from './links/links.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     LinksModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
